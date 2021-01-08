d8ba87139c6045819a868958071851b406a3b939
Eilert Vetle Forland <ev.forland@gmail.com>
2021-01-08 14:59:47 +0100

    mapping through Homepage.Json file to display info

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import HomeContent from './HomeContent';
 
 function Home() {
-	return <h1>Home page</h1>;
+	return (
+		<>
+			<h1>Home page</h1>
+			<HomeContent />
+		</>
+	);
 }
 
 export default Home;
--- src/components/home/HomeContent.js
+++ src/components/home/HomeContent.js
@@ -1,1 +1,28 @@
 import React from 'react';
+/*import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';*/
+import HomeData from '../../data/Homepage.json';
+
+function HomeContent() {
+	return (
+		<div>
+			{HomeData.map((HomeDetail, index) => {
+				return (
+					<div key={index}>
+						<h1>{HomeDetail.title} </h1>
+						<p>{HomeDetail.information} </p>
+					</div>
+				);
+			})}
+		</div>
+	);
+}
+
+export default HomeContent;
+/*<Col>
+				<Card>
+					<Card.Title>Hello</Card.Title>
+				</Card>
+			</Col>
+		</Row>*/
