dce5baa830b126a4761dacb00781d5fcaa3e7b7d
Eilert Vetle Forland <ev.forland@gmail.com>
2021-01-11 16:19:48 +0100

    added logo to header folder

--- src/components/layout/header/Nav.js
+++ src/components/layout/header/Nav.js
@@ -4,54 +4,42 @@ import { AuthContext } from '../../../context/AuthContext';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Logout from '../../auth/Logout';
+import Logo from './logo.png';
 
 function NavBar() {
-    const { user } = useContext(AuthContext);
-    return ( <
-        Navbar bg = 'dark'
-        variant = 'dark'
-        expand = 'lg' >
-        <
-        NavLink to = '/'
-        exact >
-        <
-        Navbar.Brand > Holidaze < /Navbar.Brand> <
-        /NavLink> <
-        Navbar.Toggle aria - controls = 'basic-navbar-nav' / >
-        <
-        Navbar.Collapse id = 'basic-navbar-nav' >
-        <
-        Nav className = 'mr-auto' >
-        <
-        NavLink to = '/'
-        exact >
-        Home <
-        /NavLink> <
-        NavLink to = '/accommodation'
-        exact >
-        Accommodation <
-        /NavLink> <
-        NavLink to = '/contact'
-        exact >
-        Contact <
-        /NavLink> <
-        /Nav>
+	const { user } = useContext(AuthContext);
+	return (
+		<Navbar bg='dark' variant='dark' expand='lg'>
+			<NavLink to='/' exact>
+				<Navbar.Brand>
+					<img className='logo' src={Logo} alt='Logo' />
+				</Navbar.Brand>
+			</NavLink>
+			<Navbar.Toggle aria-controls='basic-navbar-nav' />
+			<Navbar.Collapse id='basic-navbar-nav'>
+				<Nav className='mr-auto'>
+					<NavLink to='/' exact>
+						Home
+					</NavLink>
+					<NavLink to='/accommodation' exact>
+						Accommodation
+					</NavLink>
+					<NavLink to='/contact' exact>
+						Contact
+					</NavLink>
+				</Nav>
 
-        {
-            user ? ( <
-                >
-                <
-                NavLink to = '/admin' > Admin < /NavLink> <
-                Logout / >
-                <
-                />
-            ) : ( <
-                NavLink to = '/register' > Login < /NavLink>
-            )
-        } <
-        /Navbar.Collapse> <
-        /Navbar>
-    );
+				{user ? (
+					<>
+						<NavLink to='/admin'>Admin</NavLink>
+						<Logout />
+					</>
+				) : (
+					<NavLink to='/register'>Login</NavLink>
+				)}
+			</Navbar.Collapse>
+		</Navbar>
+	);
 }
 
 export default NavBar;
